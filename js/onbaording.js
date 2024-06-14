d4a6285807a3a33c26e79f210c0bef75df655abc
Ali <aliwebdevf@gmail.com>
2024-06-14 09:55:17 +0330

    .

---
+++ js/onbaording.js
@@ -0,0 +1,14 @@
+if (localStorage.getItem("loginSuccess")) {
+  localStorage.removeItem("loginSuccess");
+
+  const toastEl = document.getElementById("successToast");
+  const toast = new bootstrap.Toast(toastEl, {
+    animation: true,
+    autohide: true,
+    delay: 1000,
+  });
+  toast.show();
+}
+
+const data = localStorage.getItem("loginSuccess");
+console.log(data);
